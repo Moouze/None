ea5625779ec9fd3d58acb89727883e0ec35a95bc
Lucas <lucas_dev@outlook.com.br>
2024-08-18 15:46:16 -0300

    att PostagemController

--- src/main/java/com/moouuze/none/controller/PostagemController.java
+++ src/main/java/com/moouuze/none/controller/PostagemController.java
@@ -3,11 +3,9 @@ package com.moouuze.none.controller;
 import com.moouuze.none.model.Postagem;
 import com.moouuze.none.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +22,16 @@ public class PostagemController {
         return ResponseEntity.ok(postagemRepository.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable Long id){
+        return postagemRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+    }
 
 }
