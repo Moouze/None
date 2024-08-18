89acc67e398a02fbcc2efc636c0f0db17d7e9fae
Lucas <lucas_dev@outlook.com.br>
2024-08-18 15:44:32 -0300

    att PostagemRepository

--- src/main/java/com/moouuze/none/repository/PostagemRepository.java
+++ src/main/java/com/moouuze/none/repository/PostagemRepository.java
@@ -2,7 +2,13 @@ package com.moouuze.none.repository;
 
 import com.moouuze.none.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
 
 }
