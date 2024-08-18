bdcaed53566cca43871e97e87ecda420db2f07ad
Lucas <lucas_dev@outlook.com.br>
2024-08-18 15:30:05 -0300

    Classe PostagemController

---
+++ src/main/java/com/moouuze/none/controller/PostagemController.java
@@ -0,0 +1,28 @@
+package com.moouuze.none.controller;
+
+import com.moouuze.none.model.Postagem;
+import com.moouuze.none.repository.PostagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/postagens")
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class PostagemController {
+
+    @Autowired
+    private PostagemRepository postagemRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Postagem>> getAll() {
+        return ResponseEntity.ok(postagemRepository.findAll());
+    }
+
+
+}
