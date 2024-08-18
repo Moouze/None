2332c53743713dafb1204c12ef12ab467a973907
Lucas <lucas_dev@outlook.com.br>
2024-08-18 15:13:55 -0300

    Att interface PostagemRepository

--- src/main/java/com/moouuze/none/repository/PostagemRepository.java
+++ src/main/java/com/moouuze/none/repository/PostagemRepository.java
@@ -1,4 +1,8 @@
 package com.moouuze.none.repository;
 
-public interface PostagemRepository {
+import com.moouuze.none.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
 }
